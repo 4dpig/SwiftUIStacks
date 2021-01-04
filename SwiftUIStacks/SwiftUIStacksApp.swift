b332346b8b96f09053a51bf7fbba15c5baf88189
siweiZhu <thinkzhu110929@gmail.com>
2021-01-04 14:40:46 +0800

    Initial Commit

---
+++ SwiftUIStacks/SwiftUIStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStacksApp.swift
+//  SwiftUIStacks
+//
+//  Created by siweiZhu on 2021/1/4.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
